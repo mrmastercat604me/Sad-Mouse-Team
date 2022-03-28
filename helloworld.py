529c48cba5e94a10294b2910eef078c0650c3bdf
Duckling604 <35648934+mrmastercat604me@users.noreply.github.com>
2022-03-28 10:04:38 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
 import pygame
 from pygame.locals import *
+import sys
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((200,200))
 pygame.display.set_caption('Hello World')
 while True:
   for event in pygame.event.get():
-    if even.type == QUIT:
+    if event.type == QUIT:
       pygame.quit()
       sys.exit()
   pygame.display.update()
