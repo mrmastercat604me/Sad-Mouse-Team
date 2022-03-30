9929299ac089db9aaf064f79bb674f1d27074ded
LECLAIRE <9710287@graniteschools.org>
2022-03-30 08:31:57 -0600

    modified: studentsaver.py deleted: edit.py

--- studentsaver.py
+++ studentsaver.py
@@ -1,2 +1,5 @@
-while True:
-    inpt = input("Exit, Delete, Update, Read, or Create?: ")
+run = True
+while run:
+    inpt = input("[E]xit, [D]elete, [U]pdate, [R]ead, or [C]reate?: ").lower
+    if inpt == "e":
+        run = False
