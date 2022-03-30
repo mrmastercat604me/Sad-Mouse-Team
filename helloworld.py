e91b4fe2f9c6258c229b7036ee516fb3264226bc
Duckling604 <35648934+mrmastercat604me@users.noreply.github.com>
2022-03-30 07:33:44 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,10 +2,14 @@ import pygame, sys
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption('Hello World Project') #TAB NAME
+display_surface = pygame.display.set_mode((500,500)) #WINDOW SIZE
+font = pygame.font.Font(None, 32) #FONT
+text = font.render("Hello World!", True,(255,255,255),(0,0,0))
+textRect = text.get_rect()
+textRect.center = (500//2,500//2)
 while True:
-  pygame.display.set_mode((500,500)) #WINDOW SIZE
-  font = pygame.font.Font(None, 32) #FONT
-  text = font.render("Hello World!", True,(255,255,255),(0,0,0))
+  display_surface.fill((0,0,0))
+  display_surface.blit(text,textRect)
   for event in pygame.event.get():
     if event.type == QUIT: #IF QUIT, QUIT.
       pygame.quit()
