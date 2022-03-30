bc2628c7f433ef213e41a5b384b8357e36966226
LECLAIRE <9710287@graniteschools.org>
2022-03-30 09:14:55 -0600

    studentsaver.py

--- studentsaver.py
+++ studentsaver.py
@@ -4,14 +4,16 @@ file_path = r'saves.txt'
 while run:
     if os.path.exists(file_path):
         with open(file_path, 'w') as fp:
-            inpt = input("[E]xit, Delete, Update, Read Student ID here:____, or [C]reate?: ").lower
-            if inpt == "e":
+            inpt = input("[E]xit, Delete, Update, Read Student ID here:____, or [C]reate?: ")
+            if inpt == "e" or inpt == "E":
+                print("Goodbye")
                 run = False
-            if inpt == "c":
+            elif inpt == "c" or inpt == "C":
                 name = input("Name?: ")
                 id = input("Student_ID?: ")
                 gpa = input("GPA?: ")
                 fp.write(f"{id} {name} {gpa}\n")
+                print("-----------------------------")
     else:
         with open(file_path, 'x') as fp:
             pass
