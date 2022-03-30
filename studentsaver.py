4a646bfb32c29a6aacfdea83c7aaa27db0eac975
LECLAIRE <9710287@graniteschools.org>
2022-03-30 09:25:33 -0600

    studentsaver.py

--- studentsaver.py
+++ studentsaver.py
@@ -1,19 +1,17 @@
-import os
+import time
 run = True
 file_path = r'saves.txt'
 while run:
-    if os.path.exists(file_path):
-        with open(file_path, 'w') as fp:
-            inpt = input("[E]xit, Delete, Update, Read Student ID here:____, or [C]reate?: ")
-            if inpt == "e" or inpt == "E":
-                print("Goodbye")
-                run = False
-            elif inpt == "c" or inpt == "C":
-                name = input("Name?: ")
-                id = input("Student_ID?: ")
-                gpa = input("GPA?: ")
-                fp.write(f"{id} {name} {gpa}\n")
-                print("-----------------------------")
-    else:
-        with open(file_path, 'x') as fp:
-            pass
+    with open(file_path, 'w+') as fp:
+        inpt = input("[E]xit, Delete, Update, Read Student ID here:____, or [C]reate?: ")
+        if inpt == "e" or inpt == "E":
+            print("Goodbye")
+            run = False
+        elif inpt == "c" or inpt == "C":
+            name = input("Name?: ")
+            id = input("Student_ID?: ")
+            gpa = input("GPA?: ")
+            fp.write(f"{id} {name} {gpa}\n")
+            time.sleep(2)
+            print("Added to Database")
+            print("-----------------------------")
