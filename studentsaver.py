f0a319fb4c21be4d8880fa2e6b39fc7ffbca7245
LECLAIRE <9710287@graniteschools.org>
2022-04-11 08:13:10 -0600

    modified:   studentsaver.py

--- studentsaver.py
+++ studentsaver.py
@@ -4,9 +4,11 @@ file_path = r'saves.txt'
 while run:
     with open(file_path, 'a+') as fp:
         inpt = input("[C]reate,[E]xit, [D]elete//ID, [U]pdate//ID, [R]ead //ID: ")
+        #EXIT WORKINGS
         if inpt == "e" or inpt == "E":
             print("Goodbye")
             run = False
+        #CREATE WORKINGS
         elif inpt == "c" or inpt == "C":
             name = input("Name?: ")
             id = input("Student_ID?: ")
@@ -15,6 +17,7 @@ while run:
             time.sleep(1)
             print("Added to Database")
             print("-"*10)
+        #READ WORKINGS
         elif inpt == "r" or inpt == "R":
             with open(file_path, 'r') as fpr:
                 linecount = 0
@@ -22,4 +25,13 @@ while run:
                     pass
                 print(f"Total Lines: {linecount}")
                 id_find = input("Student Id?: ")
+
+
+            fpr.close
+        #DELETE WORKINGS
+        elif inpt == "d" or inpt == "D":
+            pass
+        #UPDATE WORKINGS
+        elif inpt == "u" or inpt == "U":
+            pass
     fp.close
