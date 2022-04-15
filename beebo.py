2888894735e36258efac898e2b9f357ba2da6edd
Ethan <mrmastercat604@gmail.com>
2022-04-15 09:18:21 -0600

    deleted:    New Text Document.txt 	deleted:    beebo.py 	deleted:    fileaslist = [].py 	deleted:    saves.txt

--- beebo.py
+++
@@ -1,9 +0,0 @@
-fileaslist = []
-with open( "New Text Document.txt", 'r') as f:
-    fileaslist = f.readlines()
-    f.close()
-with open( "New Text Document.txt", 'w') as fp:
-    for number, line in enumerate(fileaslist):
-        if number not in [1]:
-            fp.write(line)
-    fp.close
